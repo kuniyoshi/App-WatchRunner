5ed2b5ac8eacbf4ad6b98a2df03adad1f9729351
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2012-05-19 10:20:59 +0900

    initial commit

---
+++ lib/App/WatchRunner.pm
@@ -0,0 +1,32 @@
+package App::WatchRunner;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::WatchRunner -
+
+=head1 SYNOPSIS
+
+  use App::WatchRunner;
+
+=head1 DESCRIPTION
+
+App::WatchRunner is
+
+=head1 AUTHOR
+
+kuniyoshi E<lt>kuniyoshi@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
